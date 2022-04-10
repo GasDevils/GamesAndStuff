8793ce6bed6caa2eed41e27c0d02bbb6ef3866b4
Miner9er <Miner9er@GamesAndStuff>
2022-04-10 22:02:18 +0000

    Some unknown changes on index.js, package-lock.json, package.json

--- GASWebApp/index.js
+++ GASWebApp/index.js
@@ -1,5 +1,23 @@
-let express = require('express');
-let app = express();
 
-app.get('/', (req, res) => res.send('hi'));
-app.listen(420);
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/users', db.getUsers)
+//app.get('/users/:id', db.getUserById)
+//app.post('/users', db.createUser)
+//app.put('/users/:id', db.updateUser)
+//app.delete('/users/:id', db.deleteUser)
+
+
+app.get('/', (req, res) => res.send('this is an update'));
+app.listen(port);
