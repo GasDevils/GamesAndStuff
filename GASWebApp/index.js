137269e5bc47dee98ed8c182f5143bd15b0db7f9
chowy-mein <78765618+chowy-mein@users.noreply.github.com>
2022-04-10 14:46:54 -0700

    Test1
    PLEASE WORK

--- GASWebApp/index.js
+++ GASWebApp/index.js
@@ -3,3 +3,14 @@ let app = express();
 
 app.get('/', (req, res) => res.send('hi'));
 app.listen(420);
+
+<html>
+    <head>
+        <title>G.A.S.</title>
+        <link rel="stylesheet" href="style.css"></link>
+    </head>
+    <body>
+        <div class="main">
+        </div>
+    </body>
+</html>
