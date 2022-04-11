1450770ec7f61a201007ade3c38566b6410d1e34
chowy-mein <78765618+chowy-mein@users.noreply.github.com>
2022-04-11 02:16:45 -0700

    Create Homepage header

--- GASWebApp/client/src/components/navbar/Navbar.jsx
+++ GASWebApp/client/src/components/navbar/Navbar.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+
+import logo from '../../assets/logo.png'
+
 import './navbar.css';
+
+/*BEM naming convention, block element modifier*/
 
 const Navbar = () => {
   return (
-    <div>
-        Navbar
+    <div className="gpt3__navbar">
+        <div className="gpt3__navbar-links">
+          <div className="gpt3__navbar-links_logo">
+            <img src= {logo} alt= "logo" />
+          </div>
+
+/*note: change #home to point to things like "feature" and etc so they can go to correct places*/
+
+          <div className="gpt3_navbar-links_container">
+            <p><a href="#home">Home</a></p>
+            <p><a href="#home">GameGallery</a></p>
+            <p><a href="#home">MyAccount</a></p>
+            <p><a href="#home">Friend</a></p>
+
+          </div>
+
+        </div>
+
+        /*formatted properly in navbar.css*/
+        <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+        </div>
+
     </div>
   )
 }
