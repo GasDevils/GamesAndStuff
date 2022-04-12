0d6dcf290886113f79ebe053db37f092040aa66f
chowy-mein <78765618+chowy-mein@users.noreply.github.com>
2022-04-11 22:34:57 -0700

    Added test Home and Links

--- GASWebApp/client/src/App.js
+++ GASWebApp/client/src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 //to import container folder and component folder
 import {Footer, Blog, Possibility, Features, WhatGAS, Header} from './containers'
 import {CTA, Brand, Navbar} from './components';
+import Home from './Home';
+import Link1 from './Link1';
+import Link2 from './Link2';
 
 function App() {
   return (
 	<div className="App">
      <div className="gradient__bg">
-      <Navbar />
+       <Navbar />
+      <Home />
+      <Link1 />
+      <Link2 />
 
      </div>
   </div>
---
+++ GASWebApp/client/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from './components';
+function Home(){
+    return(
+        <div>
+            <Navbar />
+            <h1>Hey shitass want to see me speed bridge?</h1>
+        </div>
+    );
+}
+
+export default Home;
