dffb6b470cb148f32dfafd06543f126b8d42e990
chowy-mein <78765618+chowy-mein@users.noreply.github.com>
2022-04-11 22:44:34 -0700

    Revert "Add paths"
    This reverts commit e860d9fc39403d6cbd7bdbd9acb5cd909b62af46.

--- GASWebApp/client/src/App.js
+++ GASWebApp/client/src/App.js
@@ -6,16 +6,15 @@ import {CTA, Brand, Navbar} from './components';
 import Home from './Home';
 import Link1 from './Link1';
 import Link2 from './Link2';
-import {Route, Link} from 'react-router-dom';
 
 function App() {
   return (
 	<div className="App">
      <div className="gradient__bg">
-       <Route exact path="/" component={Home} />
-       <Route exact path="/link1" component={Link1} />
-       <Route exact path="/link2" component={Link2} />
-
+       <Navbar />
+      <Home />
+      <Link1 />
+      <Link2 />
 
      </div>
   </div>
