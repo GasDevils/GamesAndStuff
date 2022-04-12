79271e4963a101d60feb1c98ccf0fa310c6e90f6
chowy-mein <78765618+chowy-mein@users.noreply.github.com>
2022-04-11 22:21:36 -0700

    Revert "Router Dependency and Hyperlinks"
    This reverts commit 7ff83c850b8f0784d3c1b1f68aa89e749a2723ec.

--- GASWebApp/client/src/App.js
+++ GASWebApp/client/src/App.js
@@ -4,25 +4,10 @@ import './App.css';
 import {Footer, Blog, Possibility, Features, WhatGAS, Header} from './containers'
 import {CTA, Brand, Navbar} from './components';
 
-import './navbar.css';
-import Login from './Login';
-import App from './App';
-import Friends from './Friends';
-import MyAccount from './MyAccount';
-import GameGallery from './GameGallery';
-import {Route, Link} from 'react-router-dom';
-
 function App() {
   return (
 	<div className="App">
-
-        <Route exact path="/" component={App} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/gamegallery" component={GameGallery} />
-        <Route exact path="/myaccount" component={MyAccount} />
-        <Route exact path="/friends" component={Friends} />
-
-      <div className="gradient__bg">
+     <div className="gradient__bg">
       <Navbar />
 
      </div>
