1114191e01f8d5afe1b79def5dca549750461431
chowy-mein <78765618+chowy-mein@users.noreply.github.com>
2022-04-11 23:00:20 -0700

    Add Router

--- GASWebApp/client/src/App.js
+++ GASWebApp/client/src/App.js
@@ -10,9 +10,13 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
+
+    
     <div className="App">
       <Navbar />
     </div>
+    </Router>
 );
 }
 
