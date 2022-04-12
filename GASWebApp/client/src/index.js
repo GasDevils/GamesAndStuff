5b78e64488c281256d7d31cf39e63cb0fa262b27
chowy-mein <78765618+chowy-mein@users.noreply.github.com>
2022-04-11 22:27:15 -0700

    Revert "Revert "Revert "Router Dependency and Hyperlinks"""
    This reverts commit 2ee688ca8f613cc92c7d20d4ed9967a38fac429e.

--- GASWebApp/client/src/index.js
+++ GASWebApp/client/src/index.js
@@ -5,18 +5,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import {BrowserRouter} from 'react-router-dom';
-
 ReactDOM.render(
-
-  <BrowserRouter>
-  
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-
-  </BrowserRouter>,
-
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
