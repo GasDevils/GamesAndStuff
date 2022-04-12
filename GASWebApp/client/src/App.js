1f68193d5474e66194c0f351884eab5e2523016d
chowy-mein <78765618+chowy-mein@users.noreply.github.com>
2022-04-11 22:27:01 -0700

    Revert "Update App.js"
    This reverts commit 0f8013abb7f37d16b1bab7b76bcef7fda643000f.

--- GASWebApp/client/src/App.js
+++ GASWebApp/client/src/App.js
@@ -5,12 +5,22 @@ import {Footer, Blog, Possibility, Features, WhatGAS, Header} from './containers
 import {CTA, Brand, Navbar} from './components';
 
 
+import Login from './Login';
+
+import Friends from './Friends';
+import MyAccount from './MyAccount';
+import GameGallery from './GameGallery';
+import {Route, Link} from 'react-router-dom';
+
 function App() {
   return (
 	<div className="App">
 
         
-
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/gamegallery" component={GameGallery} />
+        <Route exact path="/myaccount" component={MyAccount} />
+        <Route exact path="/friends" component={Friends} />
 
       <div className="gradient__bg">
       <Navbar />
