064b800e3e3ae83be6abdc50ca9a74832493b46d
chowy-mein <78765618+chowy-mein@users.noreply.github.com>
2022-04-11 22:52:30 -0700

    Add paths 2.0

--- GASWebApp/client/src/App.js
+++ GASWebApp/client/src/App.js
@@ -6,10 +6,29 @@ import {CTA, Brand, Navbar} from './components';
 import Home from './Home';
 import Link1 from './Link1';
 import Link2 from './Link2';
+import BrowserRouter from 'react-router-dom';
 
 function App() {
   return (
-	<div className="App">
+    <div className="App">
+      	<BrowserRouter>
+    <Navbar />
+  </BrowserRouter>
+  <BrowserRouter>
+    <Route exact path='/' element={<Home />} />
+    <Route path ='link1' element={<Link1 />} />
+    <Route path ='link2' element={<Link2 />} />
+  </BrowserRouter>
+
+    </div>
+
+);
+}
+
+export default App;
+
+/*
+<div className="App">
      <div className="gradient__bg">
        <Navbar />
       <Home />
@@ -18,7 +37,4 @@ function App() {
 
      </div>
   </div>
-);
-}
-
-export default App;
+*/
