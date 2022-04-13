22add8993a7c2352bf113078d87751e1f8e21916
Jc <jchizarsa@gmail.com>
2022-04-13 01:21:32 -0700

    Create GameGallery.js

---
+++ GASWebApp/client/src/components/gamegallery/GameGallery.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GameGallery = () => {
+    return(
+        <div>
+            <h1>www.loltyler1.com discountcode: Alpha</h1>
+        </div>
+    );
+}
+
+export default GameGallery;
