fdfb48a417c97e6fe7b0b2be87cf46647ca50504
Jc <jchizarsa@gmail.com>
2022-04-12 23:41:43 -0700

    Update Home.js

--- GASWebApp/client/src/components/home/Home.js
+++ GASWebApp/client/src/components/home/Home.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import Navbar from './components/navbar/Navbar';
 
 const Home = () => {
     return(
         <div>
-            <Navbar />
             <h1>Hey shitass want to see me speed bridge?</h1>
         </div>
     );
