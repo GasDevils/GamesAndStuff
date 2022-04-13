bf2839a0f6ac3ada46f3b4cf61c8aa2d5fc0e3e4
Jc <jchizarsa@gmail.com>
2022-04-13 00:06:23 -0700

    Update App.js

--- GASWebApp/client/src/App.js
+++ GASWebApp/client/src/App.js
@@ -4,18 +4,20 @@ import React from 'react';
 
 import Navbar from './components/navbar/Navbar';
 import Home from './components/home/Home';
-import {Routes, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 const App = () => {
   return (
+    <Router>
       <div className="App">
-        <Routes>
+        <Navbar />
+        <Switch>
           <Route exact path ='/'>
-            <Navbar />
             <Home />
           </Route>
-        </Routes>
+        </Switch>
       </div>
+    </Router>
 );
 }
 
