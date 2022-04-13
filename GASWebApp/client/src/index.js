05f6f57e81ed18c1e2285a911906a5a3926b8c0c
Jc <jchizarsa@gmail.com>
2022-04-13 00:49:05 -0700

    Update index.js

--- GASWebApp/client/src/index.js
+++ GASWebApp/client/src/index.js
@@ -8,9 +8,6 @@ import reportWebVitals from './reportWebVitals';
 const container = document.getElementById('app');
 
 // Create a root.
-const root = ReactDOMClient.createRoot(container);
-
-// Initial render: Render an element to the root.
-root.render(<App tab="home" />);
+const root = ReactDOM.render(<App />, container);
 
 
