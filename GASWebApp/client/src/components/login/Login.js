388c3091a12390927f2da94fa16d651c06ba2807
chowy-mein <78765618+chowy-mein@users.noreply.github.com>
2022-04-14 23:23:49 -0700

    delete BottomText

--- GASWebApp/client/src/components/login/Login.js
+++ GASWebApp/client/src/components/login/Login.js
@@ -7,30 +7,33 @@ const Login = ()=>{
     <div className="login-box">
     <h2>Welcome G.A.S. User!</h2>
     <h2>Sign in to your account.</h2>
+        
         <form>
-        <div className="user-box">
-            <input type="text" name="" required="">
-            </input>
-            <label>Username</label>
-        </div>
-        <div className="user-box">
-            <input type="password" name="" required="">
-            </input>
-            <label>Password</label>
-        </div>
-        <a href="#">
-            <center>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            Login
-            </center>
-        </a>
-            <p style="color:white">Don't have an account?</p>
-            <p style="color:white"><u>Sign up here</u> </p>
-            
+            <div className="user-box">
+                <input type="text" name="" required="">
+                </input>
+                <label>Username</label>
+            </div>
+
+            <div className="user-box">
+                <input type="password" name="" required="">
+                </input>
+                <label>Password</label>
+            </div>
+
+            <a href="#">
+                <center>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Login
+                </center>
+            </a>
+
+
         </form>
+
     </div>)
 }
 
