3929eb95c7207c2d690b7fce8952d112a20b43fe
EvanCrabtree <75501111+EvanCrabtree@users.noreply.github.com>
2022-04-15 21:52:38 -0700

    yer

--- GASWebApp/server/queries.js
+++ GASWebApp/server/queries.js
@@ -6,8 +6,9 @@ const pool = new Pool({
     port: 8888,
 })
 
+
 const getUsers = (request, response) => {
-  pool.query('SELECT * FROM gamers ORDER BY userid ASC', (error, results) => {
+  pool.query('SELECT userID, username, dateCreated  FROM gamers ORDER BY userid ASC', (error, results) => {
     if (error) {
       throw error
     }
@@ -18,7 +19,17 @@ const getUsers = (request, response) => {
 const getUserById = (request, response) => {
   const id = parseInt(request.params.id)
 
-  pool.query('SELECT * FROM gamers WHERE userid = $1', [id], (error, results) => {
+  pool.query('SELECT userID, username, dateCreated FROM gamers WHERE userid = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const loginCheck = (request, response) => {
+  const { username, password } = request.body
+  pool.query('SELECT userID, username, dateCreated FROM gamers WHERE username = $1 AND password = $2', [username, password], (error, results) => {
     if (error) {
       throw error
     }
@@ -29,7 +40,7 @@ const getUserById = (request, response) => {
 const createUser = (request, response) => {
   const { name, password} = request.body
 
-  pool.query('INSERT INTO gamers (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+  pool.query('INSERT INTO gamers (name, password, dateCreated) VALUES ($1, $2, (to_timestamp(${Date.now()}) / 1000.0))', [name, password], (error, results) => {
     if (error) {
       throw error
     }
