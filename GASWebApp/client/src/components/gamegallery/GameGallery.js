d4575c56d129f5d0a435e5d55a3e79daf81ed7ed
Jc <jchizarsa@gmail.com>
2022-04-19 22:01:28 -0700

    Update GameGallery.js

--- GASWebApp/client/src/components/gamegallery/GameGallery.js
+++ GASWebApp/client/src/components/gamegallery/GameGallery.js
@@ -2,22 +2,11 @@ import React from 'react'
 import './gamegallery.css'
 
 const GameGallery = () => {
-    constructor(){
-        super();
-        this.state = {
-            games: []
-        }
-    }
 
-    componentDidMount(){
-        fetch('/api/games')
-        .then(res => res.json())
-        .then(games => this.setState({games}, () => console.log('Games fetched...', games)))
-    }
 
     return(
         <div>
-            
+            <h1>Just about mc'fukin had it</h1>
         </div>
     );
 }
