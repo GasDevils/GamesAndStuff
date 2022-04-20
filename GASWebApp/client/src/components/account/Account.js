551ee2d7f4ac47c89372641c1f6fb53b8d8c3a7a
chowy-mein <78765618+chowy-mein@users.noreply.github.com>
2022-04-19 21:26:43 -0700

    Navbar

--- GASWebApp/client/src/components/account/Account.js
+++ GASWebApp/client/src/components/account/Account.js
@@ -1,32 +1,35 @@
 import React from 'react'
+import Navbar from '../navbar/Navbar'
 
 const Account = () => {
+
     return(
-        <div>
-            <div class="content-profile-page">
-                <div class="profile-user-page card">
-                    <div class="img-user-profile">
-                        <img class="profile-bgHome" src="https://media.discordapp.net/attachments/823697744022470658/963600612627476500/gamerBanner2.png?width=873&height=270" />
-                        <img class="avatar" src="https://media.discordapp.net/attachments/823697744022470658/963595725181177877/b8fe2363a39908a287e1a29c136202c9.png?width=415&height=415" alt="GamerPic"/>
-                    </div>
+        <><Navbar>
+        </Navbar><div>
+                <div class="content-profile-page">
+                    <div class="profile-user-page card">
+                        <div class="img-user-profile">
+                            <img class="profile-bgHome" src="https://media.discordapp.net/attachments/823697744022470658/963600612627476500/gamerBanner2.png?width=873&height=270" />
+                            <img class="avatar" src="https://media.discordapp.net/attachments/823697744022470658/963595725181177877/b8fe2363a39908a287e1a29c136202c9.png?width=415&height=415" alt="GamerPic" />
+                        </div>
 
-                    <button>+ Friend</button>
+                        <button>+ Friend</button>
 
-                    <div class="user-profile-data">
-                        <h1>Username</h1>
-                    </div> 
-                        
-                    <div class="description-profile">I am an Arizona State University student. I love board games, tabletop games, and pizza!
+                        <div class="user-profile-data">
+                            <h1>Username</h1>
+                        </div>
+
+                        <div class="description-profile">I am an Arizona State University student. I love board games, tabletop games, and pizza!
+                        </div>
+
+                        <ul class="data-user">
+                            <li><a><strong>Games</strong><span>3390</span></a></li>
+                            <li><a><strong>Wishlist</strong><span>718</span></a></li>
+                            <li><a><strong>Friends</strong><span>239</span></a></li>
+                        </ul>
                     </div>
-                    
-                    <ul class="data-user">
-                        <li><a><strong>Games</strong><span>3390</span></a></li>
-                        <li><a><strong>Wishlist</strong><span>718</span></a></li>
-                        <li><a><strong>Friends</strong><span>239</span></a></li>
-                    </ul>
                 </div>
-            </div>
-        </div>
+            </div></>
     );
 }
 
