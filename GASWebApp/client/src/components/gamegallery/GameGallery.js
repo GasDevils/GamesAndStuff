af4631bbbf2aa667a8ff5121e1701ff3bdd524de
Jc <jchizarsa@gmail.com>
2022-04-22 02:02:52 -0700

    Update GameGallery.js

--- GASWebApp/client/src/components/gamegallery/GameGallery.js
+++ GASWebApp/client/src/components/gamegallery/GameGallery.js
@@ -7,7 +7,8 @@ const GameGallery = () => {
 
 
     return(
-      <div className="list-group">
+      <div>
+        <div className="list-group">
         <table className="table table-hover table-dark">
           <thead>
             <tr className="bg-primary">
@@ -24,8 +25,9 @@ const GameGallery = () => {
             </tr>
           </tbody>
         </table> 
-
       </div>
+      </div>
+      
     );
 }
 
