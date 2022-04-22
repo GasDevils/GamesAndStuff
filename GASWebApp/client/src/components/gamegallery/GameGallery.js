e463f095b4450055ca8fc3039bb093d894eab485
Jc <jchizarsa@gmail.com>
2022-04-22 04:20:23 -0700

    test with users?

--- GASWebApp/client/src/components/gamegallery/GameGallery.js
+++ GASWebApp/client/src/components/gamegallery/GameGallery.js
@@ -9,7 +9,7 @@ const GameGallery = (props) => {
   useEffect(() => {
     async function fetchData(){
       try{
-        const response = await GameFinder.post('/games');
+        const response = await GameFinder.get('/users');
         console.log(response);
       } catch(err){}
     }
