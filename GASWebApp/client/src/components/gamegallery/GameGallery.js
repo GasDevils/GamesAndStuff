1fe7fe45e2daa6baf3675000cb50ec1af64b9afb
Jc <jchizarsa@gmail.com>
2022-04-22 03:50:22 -0700

    Revert "Update GameGallery.js"
    This reverts commit ec2fef06152f82949fcb6140e3dae9ab3c6dc7c1.

--- GASWebApp/client/src/components/gamegallery/GameGallery.js
+++ GASWebApp/client/src/components/gamegallery/GameGallery.js
@@ -10,7 +10,7 @@ const GameGallery = (props) => {
     async function fetchData(){
       try{
         const response = await GameFinder.post('/games');
-        setGames(response.data.data.Array)
+        setGames(response.data.data.games)
       } catch(err){}
     }
     fetchData();
