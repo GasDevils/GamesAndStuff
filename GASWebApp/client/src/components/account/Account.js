abe39099ba6fb35397a50e994c793f9471b46ec0
EvanCrabtree <75501111+EvanCrabtree@users.noreply.github.com>
2022-04-25 03:05:21 -0700

    Update Account.js

--- GASWebApp/client/src/components/account/Account.js
+++ GASWebApp/client/src/components/account/Account.js
@@ -5,7 +5,7 @@ import GameFinder from '../../apis/GameFinder'
 import { UserContext } from '../../context/UserContext';
 
 const Account = () => {
-    const {gamer} = useContext(UserContext);
+
     
     var numFriends = 0;
     var numGames = 0;
@@ -15,7 +15,7 @@ const Account = () => {
         console.log(numGames);
         console.log(numWishlist);
     });
-    
+
 
     return(
         <div>
@@ -48,6 +48,7 @@ const Account = () => {
 
 
 async function getValues(){
+    const {gamer} = useContext(UserContext);
     const userID = gamer.userid;
     var numFriends = 0;
     await GameFinder.post('/getFriendCount',{
