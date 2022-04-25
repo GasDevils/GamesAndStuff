dc5b75b83ddb6d47b974660e760f60b60ba63223
Jc <jchizarsa@gmail.com>
2022-04-25 02:34:21 -0700

    gonna hope this works. I need sleep

--- GASWebApp/client/src/components/gamegallery/GameDetails.js
+++ GASWebApp/client/src/components/gamegallery/GameDetails.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import GameFinder from "../../apis/GameFinder";
+import { GamesContext } from "./context/GamesContext";
 import './gamedetails.css';
 
 const GameDetails = () => {
+    const {gameid} = useParams();
+    const {selectedGame, setSelectedGame} = useContext(GamesContext);
+    
+    useEffect(() => {
+        const fetchData = async() =>{
+            try{
+                if(gameid < 27076){
+                    const response = await GameFinder.post('/getVideoGameInfoByID', {gameid});
+                }
+                else{
+                    const response = await GameFinder.post('/getTableTopGameInfoByID', {gameid});
+                }
+                console.log(response);
+                setSelectedGame(response.data);
+            } catch (err){
+                console.log(err);
+            }
+        }
+        fetchData();
+    }, [])
   return (
     <div>
       <h1>Game Details</h1>
--- GASWebApp/client/src/context/GamesContext.js
+++ GASWebApp/client/src/context/GamesContext.js
@@ -4,9 +4,12 @@ export const GamesContext = createContext();
 
 export const GamesContextProvider = (props) => {
     const[games, setGames] = useState([]);
+    const [selectedGame, setSelectedGame] = useState([]);
 
     return(
-        <GamesContext.Provider value={{games, setGames}}>
+        <GamesContext.Provider value={{
+            games, setGames,
+            selectedGame, setSelectedGame}}>
             {props.children}
         </GamesContext.Provider>
     );
