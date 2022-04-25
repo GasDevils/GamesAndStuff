cacf1e66c12674aa0c75ee726e64e866e2fb2983
Jc <jchizarsa@gmail.com>
2022-04-25 04:58:02 -0700

    Update GameDetails.js

--- GASWebApp/client/src/components/gamegallery/GameDetails.js
+++ GASWebApp/client/src/components/gamegallery/GameDetails.js
@@ -43,7 +43,7 @@ const GameDetails = () => {
                   </tr>
             );
         } else{
-            tableTopGameColumns();
+            {tableTopGameColumns()};
         }
 
         
