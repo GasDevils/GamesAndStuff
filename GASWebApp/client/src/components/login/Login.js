d37401d12c31d461c8e7a7c1693172ea37653ce8
EvanCrabtree <75501111+EvanCrabtree@users.noreply.github.com>
2022-04-25 22:22:44 -0700

    CSS make thing pretty
    + reset context when login page is loaded

--- GASWebApp/client/src/components/login/Login.js
+++ GASWebApp/client/src/components/login/Login.js
@@ -11,7 +11,7 @@ const Login = (props)=>{
     const [loginErrors, setloginErrors] = useState('');
     const {setGamer} = useContext(UserContext);
     const history = useNavigate();
-   
+    setGamer({});
         
     const useHandleSubmit = (e) => {
         e.preventDefault();
