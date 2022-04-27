7b4eb4a7790565628f8220e0f1307ab90f4bed80
Jc <jchizarsa@gmail.com>
2022-04-26 18:32:31 -0700

    remove friends. Link wishlist and collection

--- GASWebApp/client/src/components/account/Account.js
+++ GASWebApp/client/src/components/account/Account.js
@@ -47,8 +47,8 @@ const Account = () => {
                         <img className="profile-bgHome" src="https://media.discordapp.net/attachments/823697744022470658/963600612627476500/gamerBanner2.png?width=873&height=270" />
                         <img className="avatar" src="https://media.discordapp.net/attachments/823697744022470658/963595725181177877/b8fe2363a39908a287e1a29c136202c9.png?width=415&height=415" alt="GamerPic"/>
                     </div>
-
-                    <button>+ Friend</button>
+                    
+                    {/*<button>+ Friend</button>*/}
 
                     <div className="user-profile-data">
                         <h1>{gamer.username}</h1>
@@ -58,9 +58,9 @@ const Account = () => {
                     </div>
                     
                     <ul className="data-user">
-                        <li><a><strong>Games</strong><span>{numGames}</span></a></li>
-                        <li><a><strong>Wishlist</strong><span>{numWishlist}</span></a></li>
-                        <li><a><strong>Friends</strong><span>{numFriends}</span></a></li>
+                        <li><a href="https://gasgames.tk/collection/user/"><strong>Games</strong><span>{numGames}</span></a></li>
+                        <li><a href="https://gasgames.tk/wishlist/user/"><strong>Wishlist</strong><span>{numWishlist}</span></a></li>
+                        {/*<li><a><strong>Friends</strong><span>{numFriends}</span></a></li>*/}
                     </ul>
                 </div>
             </div>
